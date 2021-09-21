62dadba892feb146957457d1f9be140c46513cf5
micxicx <micx23rus@gmail.com>
2021-09-21 14:25:23 +0400

    test_server 0.2

--- client.py
+++ client.py
@@ -1,14 +1,26 @@
 import socket
+import threading
 
 conn = socket.socket()
 conn.connect(('127.0.0.1', 9090))
-while True:
-    s =input("# ")
-    if s == "-q":
-        break
-    conn.send(s.encode('utf-8'))
 
-    data = conn.recv(1024)
-    print(data)
-conn.close()
+def send():
+    while True:
+        s =input("# ")
+        if s == "-q":
+            break
+        conn.send(s.encode('utf-8'))
+
+
+def reception():
+    print("start1")
+    while True:
+        data = conn.recv(1024)
+        print(data)
+
+th1 = threading.Thread(target=send)
+th2 = threading.Thread(target=reception)
+
+th1.start()
+th2.start()
 
--- server.py
+++ server.py
@@ -1,14 +1,45 @@
 import socket
+import threading
 
 conn = socket.socket()
 conn.bind(('', 9090))
-conn.listen(1)
-connected, adr = conn.accept()
+adrr = []
+c_conn = []
+def join(connected,adr):
+    while True:
+        try:
+            data = connected.recv(1024)
+            for i in range(len(c_conn)):
+                if c_conn[i] != connected:
+                    c_conn[i].send(data.upper())
+            if not data:
+                print("No data")
+                break
+            else:
+                print(data)
+        except ConnectionResetError:
+            print("Connect false")
+            adrr.remove(adr)
+            c_conn.remove(connected)
+            break
+    connected.close()
+def rep():
+    while True:
+        print("new")
+        conn.listen(2)
+        connected, adr = conn.accept()
+        print("conected",adr)
+        adrr.append(adr)
+        c_conn.append(connected)
+        print(1)
+        th2 = threading.Thread(target=join, args = (connected,adr))
+        print(2)
+        th2.start()
+        print(3)
 
-print("OK")
-while True:
-    data = connected.recv(1024)
-    connected.send(data.upper())
-    print("OK")
+th1 = threading.Thread(target = rep)
+th1.start()
+th1.join()
 
-connected.close()
+conn.close()
+print("Stop")
